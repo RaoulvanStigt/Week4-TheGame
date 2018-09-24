b4ebd3971c361e8c261208dc68e22f8c3424e5d9
RaoulvanStigt <raoulvanstigt@live.nl>
2018-09-24 09:49:58 +0200

    Betere versie

--- TheGame.js
+++ TheGame.js
@@ -9,7 +9,7 @@ var input = prompt("Je bent in een gevaarlijke grot gekomen. Je probeert er leve
     document.write (" <br> Je hebt gekozen voor de knuppel!")
     }
     else if  (input == "3") {
-    alert ("De kruisboog heeft geen pijlen! Daar kan je niet zo veel mee! FAILED")
+     var input = alert ("De kruisboog heeft geen pijlen! Daar kan je niet zo veel mee! FAILED")
     document.write ("<br> De kruisboog heeft geen pijlen! Daar kan je niet zo veel mee!")
 	}
     		
@@ -17,11 +17,11 @@ var input = prompt("Je bent in een gevaarlijke grot gekomen. Je probeert er leve
 var input = prompt ("Je ziet 3 deuren met manieren om dood te gaan:    1) mensen slaan je met knuppels   2) Een kogel door je hoofd    3) Je komt in een hok met leeuwen die al 3 jaar niet gegeten hebben")			
 			
 			if (input == "1") {
-    			alert ("Je probeert de overvallers te slaan, maar er zijn er te veel. Je gaat dood! FAILED")
+    			var input = alert ("Je probeert de overvallers te slaan, maar er zijn er te veel. Je gaat dood! FAILED")
     			document.write (" <br> Je probeert de overvallers te slaan, maar er zijn er te veel. Je gaat dood!")
 			}
 			else if (input == "2") {
-    			alert ("Helaas! Een kogel door je hoofd! Dat zou ik niet nog een keer doen! FAILED")
+    			var input = alert ("Helaas! Een kogel door je hoofd! Dat zou ik niet nog een keer doen! FAILED")
     			document.write (" <br> Helaas! Een kogel door je hoofd! Dat zou ik niet nog een keer doen!")
 			}
 			else if (input == "3") {
@@ -35,11 +35,11 @@ if (input == '3'){
 var input = prompt ("ONEE! Je komt een zombie tegen wat doe je?                            1) Je slaat hem met je knuppel    2) Je rent weg   3) Je blijft rustig zitten tot de zombie weg gaat")
 }	
 			if (input == "1") {
-    			alert ("Je slaat hem met de knuppel! Hij lijkt dood! Maar hij staat op! De zombie eet je hersens op! FAILED")
+    			var input = alert ("Je slaat hem met de knuppel! Hij lijkt dood! Maar hij staat op! De zombie eet je hersens op! FAILED")
     			document.write (" <br> Je hebt de goede keuze gemaakt om de knuppel te pakken! Maar het pakte niet goed uit! Je bent echt een knuppel!")
 			}
 			else if (input == "2") {
-    			alert (" Je probeert weg te rennen! Je valt over een skelet van de één leeuwen! De zombie pakt je en eet je hersens op! FAILED")
+    			var input = alert (" Je probeert weg te rennen! Je valt over een skelet van de één leeuwen! De zombie pakt je en eet je hersens op! FAILED")
 				document.write (" <br> Je probeert weg te rennen! Je valt over een skelet van de één leeuwen! De zombie pakt je en eet je hersens op!")
 			}
 			else if (input == "3") {
@@ -48,15 +48,15 @@ var input = prompt ("ONEE! Je komt een zombie tegen wat doe je?                 
 			}
 
 if (input == '3'){
-var input = prompt ("Je loopt door! Je ziet water!                                                       1) Je kan gaan zwemmen en kijken of er een uitgang is                2) Je blijft zitten. Wat doe je? ")
+var input = prompt ("Je loopt door! Je ziet water!                                                           1) Je kan gaan zwemmen en kijken of er een uitgang is                2) Je blijft zitten. Wat doe je? ")
 }
 			if (input == "1") {
-    			var zwemmen = alert ("Je besluit te gaan zwemmen. Je ziet een schaduw in het water! Je wordt bang en spartelt weg.")
+    			alert ("Je besluit te gaan zwemmen. Je ziet een schaduw in het water! Je wordt bang en spartelt weg.")
 				document.write (" <br> Je besluit te gaan zwemmen. Je ziet een schaduw in het water! Je wordt bang en spartelt weg")
 			}
 
 			else if (input == "2") {
-    			alert ("Je besluit te blijven zitten. Je wordt moe! Je gaat slapen! En je wordt nooit meer wakker! FAILED")
+    			var input = alert ("Je besluit te blijven zitten. Je wordt moe! Je gaat slapen! En je wordt nooit meer wakker! FAILED")
 				document.write (" <br> Je besluit te blijven zitten. Je wordt moe! Je gaat slapen! En je wordt nooit meer wakker!")
 			}
 
@@ -64,7 +64,7 @@ if (input == '1'){
  var input = prompt ("De schaduw is weg je kan terug zwemmen of door zwemmen. Wat doe je?                                                                      1) Terug zwemmen 2) Door zwemmen")
 }						
 						if (input == "1") {
-    							alert (" Je hebt besloten om terug te zwemmen! Het wordt donkerder in de grot! Je ziet niks! Je benen worden moe! Je hebt geen energie meer over. Je verdrinkt! FAILED ")
+    					var input = alert (" Je hebt besloten om terug te zwemmen! Het wordt donkerder in de grot! Je ziet niks! Je benen worden moe! Je hebt geen energie meer over. Je verdrinkt! FAILED ")
     					document.write (" <br> Je hebt besloten om terug te zwemmen! Het wordt donkerder in de grot! Je ziet niks! Je benen worden moe! Je hebt geen energie meer over. Je verdrinkt! ")
     					}
 
